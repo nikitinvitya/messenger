bd7dfe57e14f316e5186c94c305a7150f20e26e4
Viktar Nikitsin <nikivictor64@gmail.com>
2025-11-26 18:47:39 +0300

    feat (repository): implement getting chat last message from db

--- server/internal/dto/chatdto.go
+++ server/internal/dto/chatdto.go
@@ -5,10 +5,17 @@ import (
 	"time"
 )
 
+type LastMessage struct {
+	ID        int       `json:"id"`
+	Content   string    `json:"content"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
 type ChatResponse struct {
 	ID           int          `json:"id"`
 	Name         *string      `json:"name,omitempty"`
 	Type         string       `json:"type"`
 	CreatedAt    time.Time    `json:"created_at"`
 	Participants []model.User `json:"participants"`
+	LastMessage  *LastMessage `json:"last_message,omitempty"`
 }
