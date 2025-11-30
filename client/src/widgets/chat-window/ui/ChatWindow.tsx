07430fefe26108a27c1c4dda8be2b307fac0cb02
Viktar Nikitsin <nikivictor64@gmail.com>
2025-11-30 22:11:46 +0300

    feat (widget): create chat window, header, and message list widgets

---
+++ client/src/widgets/chat-window/ui/ChatWindow.tsx
@@ -0,0 +1,42 @@
+import classNames from 'classnames';
+import cls from './ChatWindow.module.scss'
+import {Box, Button, Text} from "@mantine/core";
+import {Message, MessageApiResponse} from "@/entities/message";
+import {ChatHeader} from "@/widgets/chat-header";
+import {MessageList} from "@/widgets/message-list";
+import {SendMessageForm} from "@/features/send-message";
+import wallpaper from "@/shared/assets/ChatWallpaper.jpg"
+
+interface ChatWindowProps {
+  className?: string;
+  initialMessages: Message[];
+  blockStatus: MessageApiResponse["blockStatus"];
+  chatID: string;
+  chatName: string;
+}
+
+export const ChatWindow = (props: ChatWindowProps) => {
+
+  const {chatID, chatName, className, initialMessages, blockStatus} = props;
+
+  const renderFooter = () => {
+    switch (blockStatus) {
+      case "recipient_blocked":
+        return <Button>Unblock</Button>
+      case "sender_blocked":
+        return <Text>You are blocked</Text>
+      default:
+        return <SendMessageForm chatID={chatID} key={chatID} />
+    }
+  }
+
+  return (
+    <Box className={classNames(cls.chatWindow)} style={{backgroundImage: `url(${wallpaper.src})`}}>
+      <ChatHeader chatName={chatName} />
+      <MessageList messages={initialMessages} />
+
+      <Box className={cls.blurFooter} />
+      {renderFooter()}
+    </Box>
+  );
+};
