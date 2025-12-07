d4f586281012a976ce6cd1c45851f9cbe04d1134
Viktar Nikitsin <nikivictor64@gmail.com>
2025-12-07 18:17:42 +0300

    refactor (api): remove mappers after backend switch to camelCase

--- client/src/entities/chat/api/getChats.ts
+++ client/src/entities/chat/api/getChats.ts
@@ -2,7 +2,6 @@ import {Chat, ChatApiResponse} from "@/entities/chat";
 import {api} from "@/shared/api";
 import {cookies} from "next/headers";
 import {JWT_TOKEN_KEY} from "@/shared/constants/cookie";
-import {mapChatsResponse} from "@/entities/chat/api/mapper";
 
 export const getChats = async (): Promise<Chat[]> => {
   const cookieStore = await cookies()
@@ -16,5 +15,6 @@ export const getChats = async (): Promise<Chat[]> => {
       Cookie: `${token.name}=${token.value}`
     }
   })
-  return mapChatsResponse(response.data)
+
+  return response.data.chats;
 }
