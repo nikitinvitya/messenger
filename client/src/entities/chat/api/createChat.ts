6ea129d89dbf44e8c0d345c0cde22099b4e49efe
Viktar Nikitsin <nikivictor64@gmail.com>
2026-04-21 16:22:35 +0300

    refactor(api): change createChat fn to make private and group chat

--- client/src/entities/chat/api/createChat.ts
+++ client/src/entities/chat/api/createChat.ts
@@ -1,10 +1,14 @@
 import {api} from "@/shared/api";
+import {Chat} from "@/entities/chat";
 
-export const createChat = async (userIDs: number[]): Promise<{ chatID: number }> => {
-  const response = await api.post<{ chatID: number }>("/chats", {
-    userIDs: userIDs,
-    chatType: "private",
-  })
+interface CreateChatPayload {
+  userIDs: number[];
+  chatType: Chat["type"];
+  name?: string;
+}
+
+export const createChat = async (payload: CreateChatPayload): Promise<{ chatID: number }> => {
+  const response = await api.post<{ chatID: number }>("/chats", payload)
 
   return response.data
 }
