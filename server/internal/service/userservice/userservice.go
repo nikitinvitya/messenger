7575f3be77ea82bd4a03ec5ecd1ed5905cf714d1
Viktar Nikitsin <nikivictor64@gmail.com>
2026-05-02 11:04:35 +0300

    feat(api): implement new endpoint with hadler to update user info

--- server/internal/service/userservice/userservice.go
+++ server/internal/service/userservice/userservice.go
@@ -2,6 +2,8 @@ package userservice
 
 import (
 	"context"
+	"errors"
+
 	"github.com/nikitinvitya/messenger/internal/model"
 	"github.com/nikitinvitya/messenger/internal/repository/userrepository"
 )
@@ -9,6 +11,7 @@ import (
 type UserService interface {
 	GetProfile(ctx context.Context, id int) (*model.User, error)
 	SearchUsersByUsername(ctx context.Context, userID int, username string) ([]model.User, error)
+	UpdateProfile(ctx context.Context, userID int, username string, bio *string, avatarURL *string) (*model.User, error)
 }
 
 type userService struct {
@@ -27,4 +30,31 @@ func (s *userService) GetProfile(ctx context.Context, id int) (*model.User, erro
 
 func (s *userService) SearchUsersByUsername(ctx context.Context, userID int, username string) ([]model.User, error) {
 	return s.repo.FindUsersByUsername(ctx, userID, username)
+}
+
+func (s *userService) UpdateProfile(ctx context.Context, userID int, username string, bio *string, avatarURL *string) (*model.User, error) {
+	currentUser, err := s.repo.GetUserByID(ctx, userID)
+	if err != nil {
+		return nil, err
+	}
+
+	if username != currentUser.Username {
+		existing, _ := s.repo.GetUserByName(ctx, username)
+		if existing != nil {
+			return nil, errors.New("username already taken")
+		}
+	}
+
+	updatedUser := &model.User{
+		ID:        userID,
+		Username:  username,
+		Bio:       bio,
+		AvatarURL: avatarURL,
+	}
+
+	if err := s.repo.UpdateUserProfile(ctx, updatedUser); err != nil {
+		return nil, err
+	}
+
+	return s.repo.GetUserByID(ctx, userID)
 }
