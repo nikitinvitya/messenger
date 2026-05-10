40e8b1b7c0431526932010bd62268d788de687af
Viktar Nikitsin <nikivictor64@gmail.com>
2026-05-10 20:48:55 +0300

    fix(api): implement server-side redirect for unauthenticated sessions

--- client/src/entities/chat/api/getChats.ts
+++ client/src/entities/chat/api/getChats.ts
@@ -2,12 +2,14 @@ import {Chat, ChatApiResponse} from "@/entities/chat";
 import {api} from "@/shared/api";
 import {cookies} from "next/headers";
 import {JWT_TOKEN_KEY} from "@/shared/constants/cookie";
+import {redirect} from "next/navigation";
+import {AppRoutes} from "@/shared/config/routes";
 
 export const getChats = async (): Promise<Chat[]> => {
   const cookieStore = await cookies()
   const token = cookieStore.get(JWT_TOKEN_KEY)
   if(!token) {
-    throw new Error('Not authenticated')
+    redirect(AppRoutes.login)
   }
 
   const response = await api.get<ChatApiResponse>(`/chats`, {
